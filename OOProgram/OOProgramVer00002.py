2a50fe45adfe05def317738370a2796114a651d6
macio-sap <18248258@qq.com>
2018-08-19 22:19:45 +0800

    备注更新，提醒自己全局变量和本地变量的差异

--- OOProgram/OOProgramVer00002.py
+++ OOProgram/OOProgramVer00002.py
@@ -5,13 +5,14 @@
 n = 0
 
 
+# 验证全局变量和本地变量的差异
 class Cls01():
 	def __init__(self):
 		global n
-		self.n = n
-		self.n += 1
-		print(n)
-		print(self.n)
+		self.n = n # 将全局变量n 的值复制给类的本地变量
+		self.n += 1 # 修改本地变量的信息
+		print(n)    # 显示全局变量n的值 应该是0
+		print(self.n) # 显示本地变量的值，应该是修改后的1
 		
 
 
